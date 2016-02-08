73d8389b9c22a9960753911644b0c01ddc6adc0a
Dominic Giglio <humanshell@gmail.com>
2016-02-08 09:47:36 -0500

    updated glisy.c comments

--- src/glisy.c
+++ src/glisy.c
@@ -1,10 +1,18 @@
 #include <glisy/glisy.h>
 
+/**
+ * Initializes glisy.
+ * Returns 1 for success, 0 for failure.
+ */
+
 int
 glisy_init () {
   return GL_TRUE;
 }
 
+/**
+ * Terminate glisy.
+ */
+
 void
-glisy_terminate (void) {
-}
+glisy_terminate (void) {}
