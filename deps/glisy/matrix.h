7970e872711e4aac29beb9e0c14b41b40d30f141
Joseph Werle <joseph.werle@gmail.com>
2016-07-27 13:17:21 -0400

    cleanup

--- deps/glisy/matrix.h
+++
@@ -1,9 +0,0 @@
-#ifndef GLISY_MATRIX_H
-#define GLISY_MATRIX_H
-
-#include <glisy/mat2.h>
-#include <glisy/mat3.h>
-#include <glisy/mat4.h>
-#include <glisy/quat.h>
-
-#endif
--- deps/glisy/vector.h
+++
@@ -1,8 +0,0 @@
-#ifndef GLISY_VECTOR_H
-#define GLISY_VECTOR_H
-
-#include <glisy/vec2.h>
-#include <glisy/vec3.h>
-#include <glisy/vec4.h>
-
-#endif
